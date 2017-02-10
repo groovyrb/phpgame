c5d5a82b94bd73ccb4235537bf890da60681bfee
groovyrb <rahuraj225@gmail.com>
2017-02-10 19:18:40 +0530

    project file added

---
+++ index.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<title>Random Number Generator</title>
+
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
+	<div class='container'>
+	<div class='row '>
+		<h3>A Simple Number Game</h3>
+			<div class='well col-md-6'>
+				<div class='btn btn-success free' id='number'>9</div>
+				<div class='btn btn-success free' id='number1'>9</div>
+				<div class='btn btn-success free' id='number2'>9</div>
+			</div>
+			<div class='col-md-6 '>
+				<img src='images/smiley-106.gif' width='100px' height='100px' id='won' style='display:none' title='Great Job'/>
+				<img src='images/impatient1.gif' width='100px' height='100px' id='shit' style='display:none' title='Just Missed It'/>
+				<img src='images/Facepalm-smiley-animated.gif' width='100px' height='100px' id='oops' style='display:none' title='Oops Try Again'/>
+			</div>
+	
+			</div>
+	<div><a class='btn btn-danger' onclick="gamestart('')">Start </a> <button class='btn btn-danger' id='result' onclick="myStopFunction();result();">Stop </button></div></div>		
+	
+	
+	
+	</div>
+	
+	
+<style>
+.free {
+    color: #fff;
+    background-color: #5cb85c;
+    border-color: #4cae4c;
+    font-size: -webkit-xxx-large;
+    padding: 60px;
+}
+</style>	
+	
+	
+<script>
+/* Function Random generator*/
+
+function gamestart(option){
+
+ window.myVar = setInterval(function(){ myTimer() }, 100);
+
+	}
+	
+	/*Number generator*/
+function myTimer() {
+	var x = Math.floor((Math.random() * 10) );
+	var y = Math.floor((Math.random() * 9) );
+	var z = Math.floor((Math.random() * 8));
+	
+    var d = document.getElementById("number").innerHTML = x;
+    var d = document.getElementById("number1").innerHTML = y;
+    var d = document.getElementById("number2").innerHTML = z;
+}
+
+/*Stop Timer*/
+function myStopFunction() {
+		
+    clearInterval(myVar);
+	
+}
+/*Result Function */
+function result(){
+	document.getElementById("won").style.display = "none";
+	document.getElementById("shit").style.display = "none";
+	document.getElementById("oops").style.display = "none";
+	var x = document.getElementById("number");
+	x = x.textContent;
+    var y = document.getElementById("number1");
+    y = y.textContent;
+    var z = document.getElementById("number2");
+    z = z.textContent;
+    	
+    /*Message Alert */
+    if(x==y && y==z){
+		alert('Hurrey! You Won');
+		document.getElementById("won").style.display = "block";
+	}else if(x==y || y==z || z==x){
+		alert('Just Missed it ');
+		document.getElementById("shit").style.display = "block";
+	}else{
+		alert('Oops you missed');
+		document.getElementById("oops").style.display = "block";
+	}
+}
+
+</script>
+
+</body>
+</html>
