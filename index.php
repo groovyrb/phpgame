1e0038e2e17cd6a9d9b42870ca92a98e066dd548
Rahul Rajoria <rahuraj225@gmail.com>
2017-04-15 02:49:19 +0530

    Added Header Authore

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <html>
 <head>
+<!-- 
+Authore : Rahul Rajoria
+Designation : Sr. Software Developer 
+Modified Date : 15-April-2017
+-->
 <title>Random Number Generator</title>
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
